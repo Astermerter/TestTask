d46bd9aad4306f6b1430f9770282b4aa2a32f6a8
Соснин Артём Олегович <aososnin@edu.hse.ru>
2024-10-17 01:22:19 +0500

    add *.v file samples

---
+++ test(sample).v
@@ -0,0 +1,29 @@
+module test;
+        reg [15:0] first_num;
+        reg [15:0] second_num;
+        wire [31:0] solution;
+
+        main uut(
+                .first_num(first_num),
+                .second_num(second_num),
+                .solution(solution)
+        );
+
+        initial begin
+                first_num = 123; // сделать рандомные числа в c++
+                second_num = 345;
+                #10;
+                $display("solution %d * %d is %d", first_num, second_num, solution);
+
+                first_num = 154;
+                second_num = 654;
+                #10;
+                $display("solution %d * %d is %d", first_num, second_num, solution);
+
+                first_num = 186;
+                second_num = 348;
+                #10;
+                $display("solution %d * %d is %d", first_num, second_num, solution);
+                $finish;
+        end
+endmodule
