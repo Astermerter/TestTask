506d54bfa77e48b48c93eaabf299022d254c2fb9
Соснин Артём Олегович <aososnin@edu.hse.ru>
2024-10-17 01:46:28 +0500

    little change on main logic (eg: add 4*6)

--- main(sample).v
+++ main(sample).v
@@ -3,7 +3,7 @@ module main(
         input [15:0] second_num, // тут тоже
         output reg [31:0] solution // и тут
 );
-        reg [3:0] n = 3; // тут вообще все надо правильно указать
+        reg [3:0] n = 1; // тут вообще все надо правильно указать
         reg [3:0] m;
         reg [15:0] F_F_P; // первая часть первого числа
         reg [15:0] F_S_P; // вторая часть первого числа
@@ -19,26 +19,30 @@ module main(
         integer buf2; // (10^2m)
         integer i;
         always @(*) begin
-                m = n >> 1;
-                solution = 0;
-                buf1 = 10;
-                buf2 = 10;
-                for (i = 0; i < m - 1; i++) begin
-                        buf1 = buf1 * 10;
-                end
-                for (i = 0; i < 2 * m - 1; i++) begin
-                        buf2 = buf2 * 10;
-                end
-                F_F_P = first_num / buf1;
-                F_S_P = first_num % buf1;
-                S_F_P = second_num / buf1;
-                S_S_P = second_num % buf1;
+                if (n > 1) begin
+                        m = n >> 1;
+                        solution = 0;
+                        buf1 = 10;
+                        buf2 = 10;
+                        for (i = 0; i < m - 1; i++) begin
+                                buf1 = buf1 * 10;
+                        end
+                        for (i = 0; i < 2 * m - 1; i++) begin
+                                buf2 = buf2 * 10;
+                        end
+                        F_F_P = first_num / buf1;
+                        F_S_P = first_num % buf1;
+                        S_F_P = second_num / buf1;
+                        S_S_P = second_num % buf1;
 
-                p1 = F_F_P * S_F_P;
-                p2 = F_S_P * S_S_P;
-                p3 = (F_F_P + F_S_P)*(S_F_P + S_S_P);
-                p4 = p3 - p2 - p1;
+                        p1 = F_F_P * S_F_P;
+                        p2 = F_S_P * S_S_P;
+                        p3 = (F_F_P + F_S_P)*(S_F_P + S_S_P);
+                        p4 = p3 - p2 - p1;
 
-                solution = p1 * buf2 + p4 * buf1 + p2;
+                        solution = p1 * buf2 + p4 * buf1 + p2;
+                end else begin
+                        solution = firs_num * second_num;
+                end
         end
 endmodule
